c9ac11670d42064adcaf07af7d491df7ff2960dd
Sathish <76247858+iamSathishR@users.noreply.github.com>
2025-10-02 23:37:31 +0530

    ++human readable size fn.

--- pathsnap.go
+++ pathsnap.go
@@ -18,20 +18,40 @@ func printTree(path string, prefix string, f *os.File) error {
 		if i == len(entries)-1 {
 			connector = "|__ "
 		}
-		line := fmt.Sprintf("%s %s %s\n", prefix, connector, entry.Name())
-		f.WriteString(line)
 
 		if entry.IsDir() {
+			line := fmt.Sprintf("%s %s %s/\n", prefix, connector, entry.Name())
+			f.WriteString(line)
 			newPrefix := prefix
 			if i == len(entries)-1 {
-				newPrefix += "  "
+				newPrefix += "    "
 			} else {
-				newPrefix += "│  "
+				newPrefix += " |   "
 			}
 			printTree(filepath.Join(path, entry.Name()), newPrefix, f)
+		} else {
+			info, err := entry.Info()
+			if err != nil {
+				continue
+			}
+			line := fmt.Sprintf("%s %s %s (%s)\n", prefix, connector, entry.Name(), hrSize(info.Size()))
+			f.WriteString(line)
 		}
 	}
 	return nil
+}
+
+func hrSize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.2f %cB", float64(size)/float64(div), "KMGTPE"[exp])
 }
 
 func main() {
@@ -40,28 +60,27 @@ func main() {
 		fmt.Println("Cannot get current directory:", err)
 		return
 	}
-	
+
 	snapshotFile := fmt.Sprintf("snapshot_%s.txt", time.Now().Format("20060102_150405"))
 	f, err := os.Create(snapshotFile)
 	if err != nil {
-		fmt.Println("cannot create snapshot file:", err)
+		fmt.Println("Cannot create snapshot file:", err)
 		return
 	}
 	defer f.Close()
-	
+
 	f.WriteString(dir + "\n")
 	printTree(dir, "", f)
 
-	fmt.Println("Directory snapshot saved to: ", snapshotFile)
+	fmt.Println("Directory snapshot saved to:", snapshotFile)
 }
-
 // C:\Users\PC\Downloads\GoOps
 // |__ Folder1
-// │  |__ one.go
+// │  |__ one.go (2.3 KB)
 // |__ Folder2
-// │  |__ two.go
-// |__ one.go
-// |__ pathsnap.go
-// |__ two.go
-// |__ three.go
-// |__ snapshot_20251001_233105.txt
+// │  |__ two.go (2.3 KB)
+// |__ one.go (2.3 KB)
+// |__ pathsnap.go (2.3 KB)
+// |__ two.go (2.3 KB)
+// |__ three.go (2.3 KB)
+// |__ snapshot_20251001_233105.txt (20 KB)
