24a9c66be280fede9d70a155299a340123305cac
Sathish <76247858+iamSathishR@users.noreply.github.com>
2025-10-03 23:01:32 +0530

    w/ error handling

--- txts2zip.go
+++ txts2zip.go
@@ -9,19 +9,31 @@ import (
 )
 
 func main() {
-	z, _ := os.Create("txt_backup_" + time.Now().Format("20060102_150405") + ".zip")
+	z, err := os.Create("txt_backup_" + time.Now().Format("20060102_150405") + ".zip")
+	if err != nil {
+		return
+	}
 	defer z.Close()
 	w := zip.NewWriter(z)
 	defer w.Close()
 
-	filepath.WalkDir(".", func(p string, d os.DirEntry, _ error) error {
-		if d.IsDir() || filepath.Ext(d.Name()) != ".txt" {
+	filepath.WalkDir(".", func(p string, d os.DirEntry, e error) error {
+		if e != nil || d.IsDir() || filepath.Ext(d.Name()) != ".txt" {
 			return nil
 		}
-		f, _ := os.Open(p)
+		f, err := os.Open(p)
+		if err != nil {
+			return nil
+		}
 		defer f.Close()
-		r, _ := filepath.Rel(".", p)
-		c, _ := w.Create(r)
+		rel, err := filepath.Rel(".", p)
+		if err != nil {
+			return nil
+		}
+		c, err := w.Create(rel)
+		if err != nil {
+			return nil
+		}
 		io.Copy(c, f)
 		return nil
 	})
